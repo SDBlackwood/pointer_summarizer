5ba15000aaab32be8e9c418a8dcf382d1e4dc112
Scott Blackwood <Scott.Blackwood@Outlook.com>
2021-12-16 16:52:36 +0000

    Updating eval with structural attention

--- training_ptr_gen/eval.py
+++ training_ptr_gen/eval.py
@@ -39,7 +39,7 @@ class Evaluate(object):
         dec_batch, dec_padding_mask, max_dec_len, dec_lens_var, target_batch = \
             get_output_from_batch(batch, use_cuda)
 
-        encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens, enc_padding_mask)
+        encoder_outputs, encoder_feature, encoder_hidden,ri = self.model.encoder(enc_batch, enc_lens, enc_padding_mask)
         s_t_1 = self.model.reduce_state(encoder_hidden)
 
         step_losses = []
@@ -47,7 +47,7 @@ class Evaluate(object):
             y_t_1 = dec_batch[:, di]  # Teacher forcing
             final_dist, s_t_1, c_t_1,attn_dist, p_gen, next_coverage = self.model.decoder(y_t_1, s_t_1,
                                                         encoder_outputs, encoder_feature, enc_padding_mask, c_t_1,
-                                                        extra_zeros, enc_batch_extend_vocab, coverage, di)
+                                                        extra_zeros, enc_batch_extend_vocab, coverage, di, ri)
             target = target_batch[:, di]
             gold_probs = torch.gather(final_dist, 1, target.unsqueeze(1)).squeeze()
             step_loss = -torch.log(gold_probs + config.eps)
